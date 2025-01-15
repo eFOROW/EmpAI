80eaafbbbf71b56ec9300553264a968154064a24
김원형 <kwh7515@live.wsu.ac.kr>
2025-01-15 17:23:49 +0900

    1.15오후

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,23 +2,20 @@ import { Footer, Navbar } from "../components/Home";
 import {
   About,
   Explore,
-  Hero,
-  WhatsNew,
+  Hero
 } from "../sections/Home";
 
 export default function Home() {
   return (
-    <main className="bg-gray-200 overflow-hidden">
+    <main className="bg-gray-150 overflow-hidden">
       <Navbar />
-      <Hero />
+      <div className="mt-[130px]">
+        <Hero />
+      </div>
       <section className="relative">
         <About />
         <div className="gradient-03 z-0" />
         <Explore />
-      </section>
-      <section className="relative">
-        <div className="gradient-03 z-0" />
-        <WhatsNew />
       </section>
       <Footer />
     </main>
